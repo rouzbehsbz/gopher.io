a7e8705634ed7d6c4006ce85d8d14caddb22e558
rouzbehsbz <rouzbehsbz@gmail.com>
2025-02-12 21:00:13 +0330

    feat: add packet structure

--- internal/engine.io/packet.go
+++ internal/engine.io/packet.go
@@ -1,5 +1,11 @@
 package engineio
 
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+)
+
 type PacketType int
 
 const (
@@ -13,6 +19,44 @@ const (
 )
 
 type Packet struct {
-	_type PacketType
-	data  []byte
+	Type    PacketType
+	RawData any
+}
+
+func NewPacket(_type PacketType, rawData any) *Packet {
+	return &Packet{
+		Type:    _type,
+		RawData: rawData,
+	}
+}
+
+func (p *Packet) EncodePacket(isBinarySupported bool) ([]byte, error) {
+	buffer, err := p.toBuffer()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if isBinarySupported {
+		return buffer, nil
+	}
+
+	return append([]byte("b"), []byte(base64.StdEncoding.EncodeToString(buffer))...), nil
+}
+
+func (p *Packet) toBuffer() ([]byte, error) {
+	switch v := p.RawData.(type) {
+	case []byte:
+		return v, nil
+	case string:
+		return []byte(v), nil
+	default:
+		b, err := json.Marshal(v)
+
+		if err != nil {
+			return nil, errors.New("can't marshal to json encoding.")
+		}
+
+		return b, nil
+	}
 }
